d143b425f166c30d1bd22fbb4ee665850ece8860
Sheckerol <checol70@gmail.com>
2020-01-20 23:17:44 -0700

    added instructions

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,6 +10,14 @@ class Home extends Component{
                 <p>I am currently building my portfolio. This means that lots of content is missing. I do not have the unity game hooked up to the react app, but I have created the unity game that is going to be the center of the portfolio. I am planning on making it so that if you drive the tank past one of the walls it will lead you to a different page on this app. Please come back once I have hooked that up!</p>
                 <p>Thanks,</p>
                 <p>Colton</p>
+                <h3>Instructions</h3>
+                <p>Currently Left click is HE, but will be changing soon.</p>
+                <p>Left Click: Fire Armor Piercing</p>
+                <p>Right Click: Fire High Explosive</p>
+                <p>WASD keys: drive the tank</p>
+                <p>ESC/CTRL: Unlock Mouse Cursor</p>
+                <p>To see content: Drive past initial wall placement. will bring you to react pages, or links to external pages.</p>
+
                 <Link to="/desktop">
                     <button>Mouse and Keyboard</button>
                 </Link>
