be127f9232ab13a0dad2225f3086f44f5ee49040
Colton Chevalier <checol70@gmail.com>
2020-06-26 03:13:56 -0600

    Initial try for test branch

---
+++ src/Components/WebPageLink/WebPageLink.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./WebPageLink.css";
+export const WebPageLink = (props) => (
+    <li>
+        <img src = {props.image} alt = {props.name} className = "image"></img>
+
+    </li>
+)
